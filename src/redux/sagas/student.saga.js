88372eebe1406716fec36ba4c6d6edc74cf86153
Jonathon Baxendell <j.bax626@gmail.com>
2022-12-03 13:29:42 -0600

    student reducer for student list data setup

--- src/redux/sagas/student.saga.js
+++ src/redux/sagas/student.saga.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
+//Generators
+function* fetchAllStudents() {
+    try {
+        const studentResponse = yield axios.get('/api/student');
+        yield put({ type: 'SET_LIST', payload: studentResponse.data });
+    } catch (e) {
+        console.log(e);
+        alert('Something wrong: fetchAllStudents saga');
+    }
+}
 
 
 // Watcher
 function* studentSaga() {
-    
+    yield takeLatest('FETCH_ALL_STUDENTS', fetchAllStudents);
 }
 
 export default studentSaga;
