bb20e9d5fb93e22950713f7f37a714d964a78c43
Holly Drazenovich <hollydraze14@gmail.com>
2022-12-06 12:38:10 -0600

    added a function to he stdents reports page

--- src/components/Reports/Reports.jsx
+++ src/components/Reports/Reports.jsx
@@ -1,2 +1,19 @@
 // in this page the admin on click will be able to the reports the can give quick 
 //data in regards  to student placement post grad 
+
+//mui imports 
+import Container from '@mui/material/Container';
+import { Typography } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+//
+
+function studentReports(){
+
+
+}
+
+export default studentReports;
