bb06dd16073c64a0ab889fac3d7bc2d51d080f08
Alex Suedkamp <ajsuedkamp@gmail.com>
2022-12-12 17:37:21 -0600

    placed vs unplaced pie chart

--- src/components/PieChart/PieChart.jsx
+++ src/components/PieChart/PieChart.jsx
@@ -49,12 +49,47 @@ function PieChart() {
     ],
   };
 
+  const dataPlaced = {
+    labels: ['Unplaced', 'Placed'],
+    datasets: [
+      {
+        label: 'Placement',
+        data: [allStudents.filter(student => student.placed_at === null).length,
+               allStudents.filter(student => student.placed_at !== null).length],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
 
     return (
-        <div className="chart-container">
-            <h2 style={{ textAlign: "center" }}>ISA vs Vet Tech</h2>
-            <Pie data={data}/>
+        <div>
+            <div className="chart-container">
+                <h2 style={{ textAlign: "center" }}>ISA vs Vet Tech</h2>
+                <Pie data={data}/>
+            </div>
+            <div>
+                <h2 style={{ textAlign: "center" }}>Vet Tech Placed vs Unplaced</h2>
+                <Pie data={dataPlaced} />
+            </div>
         </div>
+        
     )
 }
 
